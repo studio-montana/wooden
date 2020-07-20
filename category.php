9014419fb87209f6a3e212514fd157d1dbd35640
cyril-montana <artcyril@gmail.com>
2020-07-20 11:17:21 +0200

    MAJ category.php

--- category.php
+++ category.php
@@ -12,35 +12,37 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-		
+
 		<header class="page-header">
-			<?php single_cat_title(); ?>
+			<h1><?php echo single_cat_title(); ?></h1>
 		</header>
-		
-		<?php
-		if ( have_posts() ) :
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		<div class="page-content">
+			<?php
+			if ( have_posts() ) :
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content-resume', get_post_type() );
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-			endwhile;
+					/*
+					 * Include the Post-Type-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content-resume', get_post_type() );
 
-			the_posts_navigation();
+				endwhile;
 
-		else :
+				the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+			else :
 
-		endif;
-		?>
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+	</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
