0d08334fa75a80634e6855313c445ed723062178
seb-montana <seb-montana@users.noreply.github.com>
2020-07-29 17:10:30 +0200

    Revert "MAJ style tool Wall" => corrige les changement de permissions de fichiers non souhaités
    This reverts commit c63a449d68c883b85d454e623d691d7d315b281a.

--- category.php
+++ category.php
@@ -12,37 +12,35 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+		
 		<header class="page-header">
-			<h1><?php echo single_cat_title(); ?></h1>
+			<?php single_cat_title(); ?>
 		</header>
+		
+		<?php
+		if ( have_posts() ) :
 
-		<div class="page-content">
-			<?php
-			if ( have_posts() ) :
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
 
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content-resume', get_post_type() );
 
-					/*
-					 * Include the Post-Type-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content-resume', get_post_type() );
+			endwhile;
 
-				endwhile;
+			the_posts_navigation();
 
-				the_posts_navigation();
+		else :
 
-			else :
+			get_template_part( 'template-parts/content', 'none' );
 
-				get_template_part( 'template-parts/content', 'none' );
-
-			endif;
-			?>
-	</div>
+		endif;
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
