e50d6b5396393d5db3fb8beecfc9a59aed2f704a
seb-montana <seb-montana@users.noreply.github.com>
2020-11-21 00:42:19 +0100

    - increment version

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @package wooden
  */
 
-define('WOODEN_WEBCACHE_VERSION', '0.0.5');
+define('WOODEN_WEBCACHE_VERSION', '0.0.6');
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
